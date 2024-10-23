70ba9910fadc1af7f9037a47f534045ae16a1b77
camiloadones <camilo.adones@usm.cl>
2024-10-23 16:50:02 -0300

    sign in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default SingIn = () => {
+export default SignIn = () => {
   return (
-    <View>
+    <SafeAreaView className="h-full bg-primary ">
       <Text>sign-in</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
