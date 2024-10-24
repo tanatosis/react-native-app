e4f1cbfd08a249138806b7fd8b708354bb704080
camiloadones <camilo.adones@usm.cl>
2024-10-24 14:55:06 -0300

    form

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import {Link} from 'expo-router'
 
 export default SignIn = () => {
+const [form, setForm] = useState({
+    email: "",
+    password: "",
+});
+const [isSubmitting, setIsSubmitting] = useState(false)
+
+const submit = () =>{
+
+}
+
   return (
-    <SafeAreaView className="h-full bg-primary ">
-      <Text>sign-in</Text>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image 
+          source={images.logo}
+          resizeMode='contain'
+          className="w-[115px] h-[35px]"
+          />
+          <Text
+          className="text-2xl text-white text-semibold mt-10 font-psemibold"
+          >Iniciar sesión</Text>
+          <FormField
+          title="Correo electrónico"
+          value={form.email}
+          handleChangeText={(e) =>{
+            setForm({...form, email: e})}}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+            <FormField
+          title="Contraseña"
+          value={form.password}
+          handleChangeText={(e) =>{
+            setForm({...form, password: e})}}
+            otherStyles='mt-7'
+             />
+
+             <CustomButton 
+             title ="Iniciar sesión"
+             handlePress={submit}
+             containerStyles="mt-7"
+             isLoading={isSubmitting} 
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+               <Text className="text-gray-100 text-lg font-pregular ">
+               ¿No tienes cuenta?
+               
+                </Text>
+                <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Inscribirse</Link>
+          </View>
+        </View>
+        </ScrollView>
     </SafeAreaView>
   )
 }
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -35,11 +35,18 @@ const HomeLayout = () => {
   if (!fontsLoaded && !error) {
     return null;
   }
-
+{/*ocultar menu superior */ }
   return (
+    
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/*<Stack.Screen name="/search/[query]" options={{ headerShown: false }} />*/ }
     </Stack>
+    
+    
+
   );
 };
 
